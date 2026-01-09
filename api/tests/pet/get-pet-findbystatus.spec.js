ece907c00e275b43ff8e7f9004f13a70bd62a099
hari-prabowo <104328272+hari-prabowo@users.noreply.github.com>
2026-01-09 17:51:30 +0700

    Adding schema validator (wip) and cleaning up console logs (#2)
    * console log cleanup
    
    * (wip) adding schema and schema validator

--- api/tests/pet/get-pet-findbystatus.spec.js
+++ api/tests/pet/get-pet-findbystatus.spec.js
@@ -1,43 +1,57 @@
 const { test, expect } = require('@playwright/test');
 
-test('should only return pets with the status=available', async ({ request }, testInfo) => {
-    const response = await request.get('https://petstore.swagger.io/v2/pet/findByStatus?status=available');
-    expect(response.status()).toBe(200);
+test.describe('GET /api/v2/pet/findByStatus - verify getting list of pets by status', () => {
+    test('should only return pets with the status=available', async ({ request }, testInfo) => {
+        const response = await request.get('https://petstore.swagger.io/v2/pet/findByStatus?status=available');
+        expect(response.status()).toBe(200);
 
-    const resBody = await response.json();
-    await testInfo.attach('RESPONSE', {
-        body: JSON.stringify(resBody, null, 2),
-        contentType: 'application/json'
+        const resBody = await response.json();
+
+        await testInfo.attach('REQUEST', {
+            body: 'https://petstore.swagger.io/v2/pet/findByStatus?status=available',
+            contentType: 'text/plain'
+        });
+
+        await testInfo.attach('RESPONSE', {
+            body: JSON.stringify(resBody, null, 2),
+            contentType: 'application/json'
+        });
+
+        let isAllPetsAvailable = true;
+        for (const pet of resBody) {
+            if (pet.status !== 'available') {
+                isAllPetsAvailable = false;
+                break;
+            }
+        }
+
+        expect(isAllPetsAvailable).toBe(true);
     });
 
-    let isAllPetsAvailable = true;
-    for (const pet of resBody) {
-        if (pet.status !== 'available') {
-            isAllPetsAvailable = false;
-            break;
+    test('should only return pets with the status=pending', async ({ request }, testInfo) => {
+        const response = await request.get('https://petstore.swagger.io/v2/pet/findByStatus?status=pending');
+        expect(response.status()).toBe(200);
+
+        const resBody = await response.json();
+
+        await testInfo.attach('REQUEST', {
+            body: 'https://petstore.swagger.io/v2/pet/findByStatus?status=pending',
+            contentType: 'text/plain'
+        });
+
+        await testInfo.attach('RESPONSE', {
+            body: JSON.stringify(resBody, null, 2),
+            contentType: 'application/json'
+        });
+
+        let isAllPetsAvailable = true;
+        for (const pet of resBody) {
+            if (pet.status !== 'pending') {
+                isAllPetsAvailable = false;
+                break;
+            }
         }
-    }
 
-    expect(isAllPetsAvailable).toBe(true);
-});
-
-test('should only return pets with the status=pending', async ({ request }, testInfo) => {
-    const response = await request.get('https://petstore.swagger.io/v2/pet/findByStatus?status=pending');
-    expect(response.status()).toBe(200);
-
-    const resBody = await response.json();
-    await testInfo.attach('RESPONSE', {
-        body: JSON.stringify(resBody, null, 2),
-        contentType: 'application/json'
+        expect(isAllPetsAvailable).toBe(true);
     });
-
-    let isAllPetsAvailable = true;
-    for (const pet of resBody) {
-        if (pet.status !== 'pending') {
-            isAllPetsAvailable = false;
-            break;
-        }
-    }
-
-    expect(isAllPetsAvailable).toBe(true);
 });
--- api/tests/pet/get-pet-petid.spec.js
+++ api/tests/pet/get-pet-petid.spec.js
@@ -1,14 +1,22 @@
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('existing pet id should return successful response', async ({request}, testInfo) => {
-    const response = await request.get('https://petstore.swagger.io/v2/pet/177949');
-    expect(response.status()).toBe(200);
-    
-    const resBody = await response.json();
-    testInfo.attach('RESPONSE', {
-        body: JSON.stringify(resBody, null, 2),
-        contentType: 'application/json'
+test.describe('GET /v2/pet - verify getting a specific pet by id', () => {
+    test('existing pet id should return successful response', async ({ request }, testInfo) => {
+        const response = await request.get('https://petstore.swagger.io/v2/pet/177949');
+        expect(response.status()).toBe(200);
+
+        const resBody = await response.json();
+
+        await testInfo.attach('REQUEST', {
+            body: 'https://petstore.swagger.io/v2/pet/177949',
+            contentType: 'text/plain'
+        });
+
+        await testInfo.attach('RESPONSE', {
+            body: JSON.stringify(resBody, null, 2),
+            contentType: 'application/json'
+        });
+        expect(resBody.id).toBe(177949);
+
     });
-    expect(resBody.id).toBe(177949);
-
 });
