28ee37c03ac36a2c9c422f2ca10c52b5cfb19593
Hari Prabowo <hari.adhi@gmail.com>
2026-01-13 01:47:25 +0700

    added logger and refactored findbystatus test file
    added logger helper to simplify testInfo logging
    refactored findbystatus test to reuse the same function

---
+++ api/helpers/logger.js
@@ -0,0 +1,18 @@
+async function log(testInfo, name, content, contentType) {
+    if (contentType === 'text/plain') {
+        await testInfo.attach(name, {
+            body: content,
+            contentType: contentType
+        });
+    }
+    else if (contentType === 'application/json') {
+        await testInfo.attach(name, {
+            body: JSON.stringify(content, null, 2),
+            contentType: contentType
+        })
+    }
+}
+
+module.exports = {
+    log
+}
