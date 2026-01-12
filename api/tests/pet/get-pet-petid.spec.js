373eb6c2cae2bab2c36aa1eb8e90586330264205
Hari Prabowo <hari.adhi@gmail.com>
2026-01-13 01:52:47 +0700

    use new logger for get pet by id test

--- api/tests/pet/get-pet-petid.spec.js
+++ api/tests/pet/get-pet-petid.spec.js
@@ -1,36 +1,29 @@
 const { test, expect } = require('@playwright/test');
 const  {validateSchema } = require('../../helpers/schema-validator');
 const petSchema = require('./schema/pet-schema.json');
+const { log } = require('../../helpers/logger');
 const domain = process.env.BASE_URL;
 const envName = process.env.ENV;
+
+test.beforeAll(async () => {
+    console.log(`ENV: ${envName}`);
+})
 
 test.describe('GET /v2/pet - verify getting a specific pet by id', () => {
     test('request with valid pet id should return correct schema', async ({ request }, testInfo) => {
         const testId = 5000;
         const response = await request.get(`${domain}/v2/pet/${testId}`);
 
-        console.log(`ENV: ${envName}`);
-
-        await testInfo.attach('REQUEST', {
-            body: `${domain}/v2/pet/${testId}`,
-            contentType: 'text/plain'
-        });
+        log(testInfo, 'REQUEST', `${domain}/v2/pet/${testId}`, 'text/plain');
 
         const resBody = await response.json();
-
-        await testInfo.attach('RESPONSE', {
-            body: JSON.stringify(resBody, null, 2),
-            contentType: 'application/json'
-        });
+        log(testInfo, 'RESPONSE', resBody, 'application/json');
 
         expect(response.status()).toBe(200);
         expect(resBody.id).toBe(testId);
 
         const res = await validateSchema(resBody, petSchema);
-        await testInfo.attach('SCHEMA VALIDATION', {
-            body: JSON.stringify(res.resultItems, null, 2),
-            contentType: 'application/json'
-        });
+        log(testInfo, 'SCHEMA VALIDATION', res.resultItems, 'application/json');
 
         expect(res.result).toBe(true, {
             message: 'Expected response to match expected schema, see SCHEMA VALIDATION in report attachments'
