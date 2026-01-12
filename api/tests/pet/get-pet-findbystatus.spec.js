892a7e59f590ef874d71739f3ab5314d5604a5c1
hari-prabowo <104328272+hari-prabowo@users.noreply.github.com>
2026-01-13 02:14:50 +0700

    refactored tests and added logger helper (#5)
    * added logger and refactored findbystatus test file
    
    added logger helper to simplify testInfo logging
    refactored findbystatus test to reuse the same function
    
    * use new logger for get pet by id test
    
    * refactored post pet test and some cleanup

--- api/tests/pet/get-pet-findbystatus.spec.js
+++ api/tests/pet/get-pet-findbystatus.spec.js
@@ -1,63 +1,47 @@
 const { test, expect } = require('@playwright/test');
+const { log } = require('../../helpers/logger');
 const domain = process.env.BASE_URL;
 const envName = process.env.ENV;
+const api = '/v2/pet/findByStatus';
+
+async function verifyFindByStatus(request, status) {
+    const url = `${domain}${api}?status=${status}`;
+    const response = await request.get(url);
+    const resBody = await response.json();
+
+    let allStatusCorrect = true;
+    for (const pet of resBody) {
+        if (pet.status !== status) {
+            allStatusCorrect = false;
+            break;
+        }
+    }
+
+    return {result: allStatusCorrect, response: resBody};
+}
+
+test.beforeAll(async () => {
+    console.log(`ENV: ${envName}`);
+})
 
 test.describe('GET /v2/pet/findByStatus - verify getting list of pets by status', () => {
     test('should only return pets with the status=available', async ({ request }, testInfo) => {
+        const status = 'available';
+        const testResult = await verifyFindByStatus(request, status);
 
-        console.log(`ENV: ${envName}`);
-        
-        const url = `${domain}/v2/pet/findByStatus?status=available`;
-        const response = await request.get(url);
-        expect(response.status()).toBe(200);
+        log(testInfo, 'REQUEST', `GET ${domain}${api}?status=${status}`, 'text/plain');
+        log(testInfo, 'RESPONSE', testResult.response, 'application/json');
 
-        const resBody = await response.json();
-        await testInfo.attach('REQUEST', {
-            body: url,
-            contentType: 'text/plain'
-        });
-
-        await testInfo.attach('RESPONSE', {
-            body: JSON.stringify(resBody, null, 2),
-            contentType: 'application/json'
-        });
-
-        let isAllPetsAvailable = true;
-        for (const pet of resBody) {
-            if (pet.status !== 'available') {
-                isAllPetsAvailable = false;
-                break;
-            }
-        }
-
-        expect(isAllPetsAvailable).toBe(true);
+        expect(testResult.result).toBe(true);
     });
 
     test('should only return pets with the status=pending', async ({ request }, testInfo) => {
-        const url = `${domain}/v2/pet/findByStatus?status=pending`;
-        const response = await request.get(url);
-        expect(response.status()).toBe(200);
+        const status = 'pending';
+        const testResult = await verifyFindByStatus(request, status);
 
-        const resBody = await response.json();
+        log(testInfo, 'REQUEST', `GET ${domain}${api}?status=${status}`, 'text/plain');
+        log(testInfo, 'RESPONSE', testResult.response, 'application/json');
 
-        await testInfo.attach('REQUEST', {
-            body: url,
-            contentType: 'text/plain'
-        });
-
-        await testInfo.attach('RESPONSE', {
-            body: JSON.stringify(resBody, null, 2),
-            contentType: 'application/json'
-        });
-
-        let isAllPetsAvailable = true;
-        for (const pet of resBody) {
-            if (pet.status !== 'pending') {
-                isAllPetsAvailable = false;
-                break;
-            }
-        }
-
-        expect(isAllPetsAvailable).toBe(true);
+        expect(testResult.result).toBe(true);
     });
 });
